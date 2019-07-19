d33708307ed7f34dabab0bdfb371ff2450dc8e25
mre1993 <buiquangloi1993@gmal.com>
2019-07-19 17:58:51 +0700

    add repository for user and product

---
+++ app/Repositories/ProductRepository/ProductRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\ProductRepository;
+
+use App\Repositories\RepositoryInterface;
+
+/**
+ * Interface ProductRepositoryInterface
+ * @package App\Repositories\ProductRepository
+ */
+interface ProductRepositoryInterface extends RepositoryInterface
+{
+}
