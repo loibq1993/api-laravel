df3a76b3b927366a36fd35666d85e30da4250b44
loibq1993 <buiquangloi1993@gmail.com>
2019-08-08 22:57:21 +0700

    create migration for ecommerce

---
+++ database/migrations/2019_08_04_170327_alter_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->renameColumn('image', 'feature_image');
+            $table->renameColumn('active','status');
+            $table->string('slug');
+            $table->string('SKU');
+            $table->unsignedBigInteger('supplier_id');
+            $table->dropColumn('quantity');
+            $table->unsignedBigInteger('supplier_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->renameColumn('feature_image', 'image');
+        $table->renameColumn('status','active');
+        $table->dropColumn('slug');
+        $table->dropColumn('SKU');
+        $table->dropColumn('supplier_id');
+        $table->integer('quantity')->default(0);
+        $table->dropColumn('supplier_id');
+    }
+}
---
+++ database/migrations/2019_08_04_181425_create_suppliers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('company_name');
+            $table->string('short_name')->nullable();
+            $table->sring('address')->nullable();
+            $table->string('phone');
+            $table->string('fax')->nullable();
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
